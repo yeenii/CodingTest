896bd77aea92829f405252f44d2f9d9b8918f375
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-04 14:51:35 +0900

    Title: 따옴표 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241104/따옴표 출력/print-quote.cpp
+++ 241104/따옴표 출력/print-quote.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main() {
-    cout << "He says "\It's a really simple sentence"\."<<endl;
-    return 0;
+    cout << "He says \"It's a really simple sentence\"."<<endl;
+        return 0;
 }
