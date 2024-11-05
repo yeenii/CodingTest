33754a6ebbb1fce6d34a6084a6beda05dbcf4bf4
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-05 14:56:52 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241105/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 13;
+    double b = 0.165;
+
+    cout << fixed;
+    cout.precision(6);
+
+    cout << a << " * " << b << " = ";
+    
+    cout.precision(6);
+    cout << a * b;
+ 
+    return 0;
+}
