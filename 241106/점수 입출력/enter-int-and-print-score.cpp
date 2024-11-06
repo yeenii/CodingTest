fe29beb303a8535566b49a4e646b9c0d2248134d
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-06 16:31:31 +0900

    Title: 점수 입출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241106/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a >> endl;
+
+    if(a>=1 && a<=1000)
+    {
+        cout << "Your score is " << a << " point."<<endl;
+    } 
+    
+    return 0;
+}
