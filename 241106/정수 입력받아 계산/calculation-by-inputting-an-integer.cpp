b385e6984a378f3acd185db5709a6ad97e7cc9aa
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-06 16:31:36 +0900

    Title: 정수 입력받아 계산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241106/정수 입력받아 계산/calculation-by-inputting-an-integer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    cin >> a;
+
+    if(a>=1 && a<=100)
+    {
+        cout << (a * 2) + 3 <<endl;
+    }
+    return 0;
+}
