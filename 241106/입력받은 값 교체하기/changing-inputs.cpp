22391f6b628971ec76d6adaabf96db2dbc8219b2
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-06 18:05:54 +0900

    Title: 입력받은 값 교체하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241106/입력받은 값 교체하기/changing-inputs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    int temp = a;
+    a=b;
+    b=temp;
+
+    cout << a << " " << b;
+    return 0;
+}
