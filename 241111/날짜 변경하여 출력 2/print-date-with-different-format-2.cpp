5ac7742b27d4b0cfd56166b6b7f65a3099cdb664
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-11 11:37:58 +0900

    Title: 날짜 변경하여 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241111/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+
+    cout << c << "." << a << "." << b;
+    return 0;
+}
