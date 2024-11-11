418c41af82f3d0a1250362800154dd218baa5c64
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-11 14:17:26 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241111/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b, c;
+
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+
+
+    cout << "010-" << c << "-" << b;
+
+    return 0;
+}
