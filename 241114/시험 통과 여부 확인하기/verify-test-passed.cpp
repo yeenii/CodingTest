abee1d9d27392d2ff63b975d645db2d1aa8a3d6b
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-14 15:30:52 +0900

    Title: 시험 통과 여부 확인하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241114/시험 통과 여부 확인하기/verify-test-passed.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    if (n >= 80)
+    {
+        cout << "pass" << endl;
+    }
+    else
+    {
+        cout << 80 - n << " more score" << endl;
+    }
+    return 0;
+}
