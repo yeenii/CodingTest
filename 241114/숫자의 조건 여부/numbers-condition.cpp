4116a3d6b186f1fc0e4cf042c45faa16180e3a4e
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-14 15:32:17 +0900

    Title: 숫자의 조건 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241114/숫자의 조건 여부/numbers-condition.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    cin >> a;
+
+    if(a>=113)
+    {
+        cout << 1 <<endl;
+    }
+    else
+    {
+        cout << 0 <<endl;
+    }
+    return 0;
+}
