4b47a854b08b9b8f07a5dfe83f7b488c67b6f23e
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-15 14:48:03 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241115/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    cin >> a;
+
+    if(a==100)
+    {
+        cout << "pass" <<endl;
+    }
+    else{
+        cout << "failure" <<endl;
+    }
+    return 0;
+}
