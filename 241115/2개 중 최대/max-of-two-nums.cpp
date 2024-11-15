39f3f582b1aac9135bf276dc3322e4bc13baf700
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-15 14:45:59 +0900

    Title: 2개 중 최대, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241115/2개 중 최대/max-of-two-nums.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    int result;
+
+    cin >> a >> b;
+
+    result = a>b ? a:b;
+
+    cout << result;
+    
+    return 0;
+}
