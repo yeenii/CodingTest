ee4fc58e5231b053b01f80765ef05d0588d61c06
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-15 15:02:09 +0900

    Title: 삼항연산자 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241115/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+
+    cin >> a;
+
+    if(a==1)
+    {
+        cout << "t" <<endl;
+    }
+    else
+    {
+        cout << "f" <<endl;
+    }
+    return 0;
+}
