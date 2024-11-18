f10f59179896ed56e82251c2468db89b9780d873
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-18 12:17:41 +0900

    Title: 물의 상태, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241118/물의 상태/state-of-water.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    if(n<0)
+    {
+        cout << "ice" <<endl;
+    }
+    else if(n>=100)
+    {
+        cout << "vapor" <<endl;
+    }
+    else
+    {
+        cout << "water" <<endl;
+    }
+    
+    return 0;
+}
