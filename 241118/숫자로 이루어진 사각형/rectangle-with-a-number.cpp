b6d94ffbf7107126fb18527aad06c9c5d64e004b
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-18 15:20:15 +0900

    Title: 숫자로 이루어진 사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241118/숫자로 이루어진 사각형/rectangle-with-a-number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+
+    cin >> n;
+
+    int k = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (k == 9)
+            {
+                k = 0;
+            }
+            cout << ++k <<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
