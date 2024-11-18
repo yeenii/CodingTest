56ddd6b5c5ef242cbc8aa5c6c87dabeccc9270c7
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-18 15:13:31 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241118/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int m;
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << 1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
