33a24ecb43e068deb47d2d9833903d5a659eb62b
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-19 18:29:53 +0900

    Title: 함수를 이용한 369 게임, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 241119/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 241119/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -8,16 +8,28 @@ int returnVal(int a, int b)
 
     for (int i = a; i <= b; i++)
     {
+        
+
+        if (i % 3 == 0)
+        {
+            sum++;
+            continue; //3의 배수인 숫자라면 각 자리를 검사할 필요가 없으니 다음으로 바로 넘어감.
+        }
+
         string num_str = to_string(i);
 
         for (char digit_str : num_str)
         {
+            
             int num = digit_str - '0';
 
-            if ((num == 3 || num == 6 || num == 9) || num % 3 == 0)
+            if (num == 3 || num == 6 || num == 9)
             {
-                sum += 1;
+                sum++;
+                break; //3, 6, 9 중에 하나를 포함하면 바로 루프 종료. 더이상 각 자리 검사하지 않고 그 다음으로 넘어감.
             }
+            
+            
 
         }
     }
