90eaaa8861652ff7a3fd40c70a20c4ad469b66df
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-22 17:22:45 +0900

    Title: 두 수의 거듭제곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241122/두 수의 거듭제곱/two-numbers-of-squares.cpp
+++ 241122/두 수의 거듭제곱/two-numbers-of-squares.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-    int a,b;
+    int a, b;
 
     cin >> a >> b;
 
-    int result = pow(a,b);
+    int result = pow(a, b);
 
-    cout << result <<endl;
+    cout << result << endl;
     return 0;
 }
