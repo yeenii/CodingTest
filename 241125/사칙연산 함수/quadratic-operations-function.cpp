237e5d7df2857897a11199ed75e16207b7116a3a
yeenii <104376856+yeenii@users.noreply.github.com>
2024-11-25 18:14:05 +0900

    Title: 사칙연산 함수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241125/사칙연산 함수/quadratic-operations-function.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b;
+    char c;
+
+    cin >> a >> c >> b;
+
+    if(c=='+')
+    {
+        cout << a << "+" << b << "=" <<a+b << endl;
+    }
+    else if(c=='-')
+    {
+        cout << a << "-" << b << "=" <<a-b << endl;
+    }
+    else if(c=='/')
+    {
+        cout << a << "/" << b << "=" <<a/b << endl;
+    }
+    else if(c=='*')
+    {
+        cout << a << "*" << b << "=" <<a*b << endl;
+    }
+    else
+    {
+        cout << False <<endl;
+    }
+
+    return 0;
+}
