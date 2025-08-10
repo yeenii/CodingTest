362da25bc45fb6404bf2243b0ef269f87eee40d8
yeenii <104376856+yeenii@users.noreply.github.com>
2025-08-10 17:50:48 +0900

    Title: 비를 피하기, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 250810/비를 피하기/stay-out-of-rain.cpp
+++ 250810/비를 피하기/stay-out-of-rain.cpp
@@ -14,7 +14,6 @@ const int h_max = 200;
 int n, m, h;
 int grid[n_max][n_max]; //입력
 int disGrid[n_max][n_max]; //'비를 피할 수 있는 위치'부터 최단 거리 구하기
-int tempGrid[n_max][n_max]; //사람의 최단 거리만 저장
 bool visited[n_max][n_max];
 
 vector<pair<int, int>> humanPos;
@@ -23,28 +22,15 @@ vector<pair<int, int>> exitPos;
 int dr[4] = {-1, 1, 0, 0};
 int dc[4] = {0, 0, -1, 1};
 
+queue<pair<int, int>> que;
 
 bool inRange(int x, int y)
 {
 	return x >= 0 && x < n && y >= 0 && y < n;
 }
 
-void bfs(int sr, int sc, int val)
+void bfs()
 {
-	queue<pair<int, int>> que;
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			visited[i][j] = false;
-		}
-	}
-
-	que.push({sr,sc});
-	disGrid[sr][sc] = 0;
-	visited[sr][sc] = true;
-
 	while (!que.empty())
 	{
 		int currentRow = que.front().first;
@@ -107,45 +93,44 @@ int main()
 		
 	}
 
-	//tempGrid : 사람의 최단거리만 저장하는 2차원 격자
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			tempGrid[i][j]=INT_MAX;
-
+			visited[i][j] = false;
 		}
-
 	}
 
+	//거리 구하는 그리드 -1로 초기화
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			disGrid[i][j] = -1;
+		}
+	}
 
 	//2. bfs로 최단 거리 구하기
+	/* <<큐의 원리를 이해하지 못해 생긴 대참사>>
+	* 출발 위치인 '비를 피하는 곳' 3개를 한번에 0으로 처리하고, 큐에 저장
+	* 큐가 pop하면서 '비를 피하는 곳' 3개가 저장된 뒤에 주변 셀을 저장함
+	* 셀은 처음 거리 값이 저장될 때, visited가 true가 되니까, 
+	* 나중에 거리 값을 비교할 필요 없이 맨 처음에 저장된 거리 값이 최단 거리 값이 됨
+	* 사람이 없는 곳은 나중에 출력할 때, 0처리해주면 됨.
+	*/
 	// 1로는 이동 불가. 나머지는 가능
 	for (int i = 0; i < m; i++)
 	{
-		//거리 구하는 그리드 -1로 초기화
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				disGrid[i][j] = -1;
-			}
-		}
+		int sr = exitPos[i].first;
+		int sc = exitPos[i].second;
 
-		//비 피할 곳 부터 역순으로 구함
-		bfs(exitPos[i].first, exitPos[i].second, -1);//시작 좌표, 거리값
-
-		//사람의 최단 거리 구하기
-		for (int j = 0; j < h; j++)
-		{
-			
-			tempGrid[humanPos[j].first][humanPos[j].second] 
-				= min(tempGrid[humanPos[j].first][humanPos[j].second], disGrid[humanPos[j].first][humanPos[j].second]);
-
-		}
-
-
+		que.push({ sr,sc });
+		disGrid[sr][sc] = 0;
+		visited[sr][sc] = true;
 	}
+
+	//비 피할 곳 부터 역순으로 구함
+	bfs();
 
 	//3. tempGrid에서 사람 아닌 셀은 0으로 만들고
 	//결과 출력
@@ -153,12 +138,10 @@ int main()
 	{
 		for (int j = 0; j < n; j++)
 		{
-			if (grid[i][j]!=2) //사람이아니라면
-				tempGrid[i][j] = 0;
-			else if (!visited[i][j]) //사람이지만, 방문하지 않았다면
-				tempGrid[i][j] = -1;
+			if (grid[i][j] != 2) //사람이아니라면
+				disGrid[i][j] = 0;
 
-			cout << tempGrid[i][j] << " ";
+			cout << disGrid[i][j] << " ";
 		}
 		cout << endl;
 	}
