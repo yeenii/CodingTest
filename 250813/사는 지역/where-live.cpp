39296884ec0a88068b952203d3ea88b76d20e8a9
yeenii <104376856+yeenii@users.noreply.github.com>
2025-08-13 16:59:57 +0900

    Title: 사는 지역, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250813/사는 지역/where-live.cpp
+++ 250813/사는 지역/where-live.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include <cstdio>
+#include<algorithm>
 
 using namespace std;
 
@@ -22,6 +23,11 @@ public:
 
 human hu[n_max];
 
+bool compare(const human &a, const human &b)
+{
+	if (a.name != b.name)
+		return a.name > b.name; //내림차순
+}
 int main()
 {
 	//freopen("input.txt", "r", stdin);
@@ -38,9 +44,12 @@ int main()
 		hu[i] = human(name, addr, location);
 	}
 
-	cout << "name " << hu[2].name << endl;
-	cout << "addr " << hu[2].addr << endl;
-	cout << "city " << hu[2].location << endl;
+	//이름이 사전순으로 가장 느린 사람
+	sort(hu, hu + n, compare); //객체 리스트 정렬
+
+	cout << "name " << hu[0].name << endl;
+	cout << "addr " << hu[0].addr << endl;
+	cout << "city " << hu[0].location << endl;
 
 	return 0;
 }
